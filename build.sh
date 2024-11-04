d9d5ab57ed1c643f6970c707a6a05259b24ea673
amelendez <abisaimelendez@icloud.com>
2024-11-04 12:47:20 -0600

    reload

--- build.sh
+++ build.sh
@@ -6,11 +6,11 @@ pip install -r requirements.txt
 # Ejecuta las migraciones
 python manage.py migrate
 
-Crea el superusuario automáticamente
-python manage.py shell << END
-from django.contrib.auth import get_user_model
+# Crea el superusuario automáticamente
+# python manage.py shell << END
+# from django.contrib.auth import get_user_model
 
-User = get_user_model()
-if not User.objects.filter(username='amelendez').exists():
-    User.objects.create_superuser('amelendez', 'amelendez@surgepays.com', 'Jodente_03')
-END
+# User = get_user_model()
+# if not User.objects.filter(username='amelendez').exists():
+#     User.objects.create_superuser('amelendez', 'amelendez@surgepays.com', 'Jodente_03')
+# END
